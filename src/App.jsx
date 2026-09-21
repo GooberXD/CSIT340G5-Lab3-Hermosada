f5fe146facc5cec48b6a9ca6830c8d8bd2a9326c
hanzivanhermosada <omenbruh48@gmail.com>
2026-09-21 18:37:38 +0800

    1.5: course information step 5

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,69 @@
+import './App.css'
+
 const Header = (props) => {
-  return <h1>{props.course}</h1>
+  return <h1 className="course-header">{props.course.name}</h1>
 }
 
 const Part = (props) => {
   return (
-    <p>
-      {props.part.name} {props.part.exercises} units
-    </p>
+    <div className="part-item">
+      <span>{props.part.name}</span>
+      <span className="part-units">{props.part.exercises} units</span>
+    </div>
   )
 }
 
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
+      <Part part={props.course.parts[0]} />
+      <Part part={props.course.parts[1]} />
+      <Part part={props.course.parts[2]} />
     </div>
   )
 }
 
 const Total = (props) => {
   return (
-    <p>
-      <strong>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</strong>
+    <p className="total-section">
+      <strong>
+        Number of exercises:{' '}
+        {props.course.parts[0].exercises +
+          props.course.parts[1].exercises +
+          props.course.parts[2].exercises}
+      </strong>
     </p>
   )
 }
 
 const Footer = (props) => {
   return (
-    <footer>
-      <hr />
-      <p>{props.fullName} - {props.courseCode} - {props.section}</p>
+    <footer className="footer-section">
+      <p className="footer-text">
+        {props.fullName} - {props.courseCode} - {props.section}
+      </p>
     </footer>
   )
 }
 
 const App = () => {
-  const course = 'CSIT340 - Industry Elective 1'
-  const parts = [
-    {
-      name: 'CSIT327 - Information Management 2',
-      exercises: 3
-    },
-    {
-      name: 'IT317 - Project Management',
-      exercises: 3
-    },
-    {
-      name: 'IT365 - Data Analytics 1',
-      exercises: 3
-    }
-  ]
+  const course = {
+    name: 'CSIT340 - Industry Elective 1',
+    parts: [
+      {
+        name: 'CSIT327 - Information Management 2',
+        exercises: 3
+      },
+      {
+        name: 'IT317 - Project Management',
+        exercises: 3
+      },
+      {
+        name: 'IT365 - Data Analytics 1',
+        exercises: 3
+      }
+    ]
+  }
 
   const student = {
     fullName: 'Hanz Ivan Hermosada',
@@ -61,10 +72,10 @@ const App = () => {
   }
 
   return (
-    <div>
+    <div className="card">
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
       <Footer 
         fullName={student.fullName} 
         courseCode={student.courseCode} 
