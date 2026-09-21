7a4c93b51aaa996f1ea39b7eb93d2006e714efb0
hanzivanhermosada <omenbruh48@gmail.com>
2026-09-21 18:17:48 +0800

    1.2: course information step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'CSIT340 - Industry Elective 1'
-  const part1 = 'CSIT327 - Information Management 2'
-  const exercises1 = 3
-  const part2 = 'IT317 - Project Management'
-  const exercises2 = 3
-  const part3 = 'IT365 - Data Analytics 1'
-  const exercises3 = 3
+  const parts = [
+    { name: 'CSIT327 - Information Management 2', exercises: 3 },
+    { name: 'IT317 - Project Management', exercises: 3 },
+    { name: 'IT365 - Data Analytics 1', exercises: 3 },
+  ]
 
   return (
     <div>
       <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content parts={parts} />
+      <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
     </div>
   )
 }
